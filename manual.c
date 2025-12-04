75cd151757103adaaaceb82eba4999f6b91c9bb7
qemu <requests567@gmail.com>
2025-12-04 11:30:12 +0300

    Refactor comments and pointer usage in manual.c

--- manual.c
+++ manual.c
@@ -6,20 +6,20 @@ int main() { // _start but in C (Program starts here)
 
     // char = one letter, const char* = unlimited letters (string)
     double real_femboys = 69.6969; // float but better (float so bad...)
-    double *femboy = &real_femboys; // Get 'real_femboys' address
+    double *femboy = &real_femboys; // pointer to real_femboys
 
     printf("double (float but better) real_femboys > %f\n", real_femboys); // read value
-    printf("real_femboys address: %p\n", (void*)femboy); // read address via pointer
+    printf("real_femboys address: %p\n", (void*)femboy); // print pointer
 
     // float femboy = 69.6969;
-    // reads as = 69.6966
+    // reads as = 69.6966 because float is less precise
 
     return 0; // exit program
 }
 
 // compile C file as:
-// Linux:  gcc astolfo.c -o real_femboy_file
-// Windows (MinGW): gcc astolfo.c -o real_femboy_file.exe
+// Linux:  gcc manual.c -o real_femboy_file
+// Windows (MinGW): gcc manual.c -o real_femboy_file.exe
 
 // We LOVE C!
 // C — The Global Programming Language
@@ -27,4 +27,4 @@ int main() { // _start but in C (Program starts here)
 // Windows kernel — C
 // Python interpreter — C
 // C++ = C but faster
-// minus of C — bad networking in the standard library :(
+// minus of C — no high-level networking in the standard library
